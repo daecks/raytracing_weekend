fdf1f940cdd0877bc9c7888eb48e5edd58950229
Kristian Golding <daecks@gmail.com>
2019-09-09 00:25:57 -0400

    page 31

--- main.cpp
+++ main.cpp
@@ -4,16 +4,16 @@
 #include "float.h"
 #include "camera.h"
 
-vec3 reflect(const vec3& v, const vec3& n) {
-    return v - 2*dot(v,n)*n;
-}
-
 vec3 random_in_unit_sphere() {
     vec3 p;
     do {
         p = 2.0*vec3(drand48(), drand48(), drand48()) - vec3(1,1,1);
     } while (p.squared_length() >= 1.0);
     return p;
+}
+
+vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2*dot(v,n)*n;
 }
 
 class material {
@@ -23,6 +23,18 @@ class material {
                 vec3& attenuation, 
                 ray& scattered) const = 0;
 };
+
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted) {
+    vec3 uv = unit_vector(v);
+    float dt = dot(uv, n);
+    float discriminant = 1.0 - ni_over_nt*ni_over_nt*(1-dt*dt);
+    if (discriminant > 0) {
+        refracted = ni_over_nt*(uv - n*dt) - n*sqrt(discriminant);
+        return true;
+    }else{
+        return false;
+    }
+}
 
 vec3 color(const ray& r, hitable *world, int depth) {
     hit_record rec;
@@ -41,6 +53,7 @@ vec3 color(const ray& r, hitable *world, int depth) {
         return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
     }
 }
+
 class lambertian : public material {
     public:
         lambertian(const vec3& a) : albedo(a) {}
@@ -59,17 +72,49 @@ class lambertian : public material {
 
 class metal : public material {
     public:
-        metal(const vec3& a) : albedo(a) {}
+        metal(const vec3& a, float f) : albedo(a) { if (f < 1) fuzz = f; else fuzz = 1; }
         virtual bool scatter(const ray& r_in, 
                 const hit_record& rec, 
                 vec3& attenuation,
                 ray& scattered) const {
             vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-            scattered = ray(rec.p, reflected);
+            scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
         }
         vec3 albedo;
+        float fuzz;
+};
+
+
+class dielectric : public material {
+    public:
+        dielectric(float ri) : ref_idx(ri) {}
+        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+            vec3 outward_normal;
+            vec3 reflected = reflect(r_in.direction(), rec.normal);
+            float ni_over_nt;
+            attenuation = vec3(1.0, 1.0, 1.0);
+            vec3 refracted;
+            if (dot(r_in.direction(), rec.normal) > 0) {
+                outward_normal = -rec.normal;
+                ni_over_nt = ref_idx;
+            } else {
+                outward_normal = rec.normal;
+                ni_over_nt = 1.0 / ref_idx;
+            }
+
+            if ( refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
+                scattered = ray(rec.p, refracted);
+            } else {
+                scattered = ray(rec.p, reflected);
+                return false;
+            }
+            
+            return true;
+
+        }
+        float ref_idx;
 };
 
 int main(){
@@ -79,10 +124,10 @@ int main(){
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     hitable *list[4];
-    list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+    list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
     list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
-    list[3] = new sphere(vec3(-1,0,-1), 0.5, new metal(vec3(0.8, 0.8, 0.8)));
+    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3));
+    list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.5));
     hitable *world = new hitable_list(list, 4);
     camera cam;
 
