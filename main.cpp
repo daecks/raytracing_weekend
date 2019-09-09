af7702b17fa00e7d25c3bc2d74622291747691a5
Kristian Golding <daecks@gmail.com>
2019-09-08 22:52:06 -0400

    page 28

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 #include "float.h"
 #include "camera.h"
 
+vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2*dot(v,n)*n;
+}
+
 vec3 random_in_unit_sphere() {
     vec3 p;
     do {
@@ -12,11 +16,24 @@ vec3 random_in_unit_sphere() {
     return p;
 }
 
-vec3 color(const ray& r, hitable *world) {
+class material {
+    public:
+        virtual bool scatter(const ray& r_in, 
+                const hit_record& rec, 
+                vec3& attenuation, 
+                ray& scattered) const = 0;
+};
+
+vec3 color(const ray& r, hitable *world, int depth) {
     hit_record rec;
     if (world->hit(r, 0.001, MAXFLOAT, rec)) {
-        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
-        return 0.5*color( ray(rec.p, target - rec.p), world);
+        ray scattered;
+        vec3 attenuation;
+        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+            return attenuation*color(scattered, world, depth+1);
+        } else {
+            return vec3(0,0,0);
+        }
     }
     else {
         vec3 unit_direction = unit_vector(r.direction());
@@ -24,6 +41,36 @@ vec3 color(const ray& r, hitable *world) {
         return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
     }
 }
+class lambertian : public material {
+    public:
+        lambertian(const vec3& a) : albedo(a) {}
+        virtual bool scatter(const ray& r_in, 
+                const hit_record& rec, 
+                vec3& attenuation, 
+                ray& scattered) const {
+            vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+            scattered = ray(rec.p, target - rec.p);
+            attenuation = albedo;
+            return true;
+        }
+
+        vec3 albedo;
+};
+
+class metal : public material {
+    public:
+        metal(const vec3& a) : albedo(a) {}
+        virtual bool scatter(const ray& r_in, 
+                const hit_record& rec, 
+                vec3& attenuation,
+                ray& scattered) const {
+            vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+            scattered = ray(rec.p, reflected);
+            attenuation = albedo;
+            return (dot(scattered.direction(), rec.normal) > 0);
+        }
+        vec3 albedo;
+};
 
 int main(){
     int nx = 400;
@@ -31,10 +78,12 @@ int main(){
     int ns = 100;
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-    hitable *list[2];
-    list[0] = new sphere(vec3(0,0,-1), 0.5);
-    list[1] = new sphere(vec3(0,-100.5,-1), 100);
-    hitable *world = new hitable_list(list, 2);
+    hitable *list[4];
+    list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+    list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
+    list[3] = new sphere(vec3(-1,0,-1), 0.5, new metal(vec3(0.8, 0.8, 0.8)));
+    hitable *world = new hitable_list(list, 4);
     camera cam;
 
     for (int j = ny-1; j >=0; j--) {
@@ -45,7 +94,7 @@ int main(){
                 float v = float(j + drand48()) / float(ny);
                 ray r = cam.get_ray(u, v);
                 vec3 p = r.point_at_parameter(2.0);
-                col += color(r, world);
+                col += color(r, world, 0);
             }
 
             col /= float(ns);
