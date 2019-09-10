990210cce90600d3ff117b2c74fbe2019f388698
Kristian Golding <daecks@gmail.com>
2019-09-09 22:13:56 -0400

    page 39

--- main.cpp
+++ main.cpp
@@ -148,7 +148,12 @@ int main(){
     list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5));
 
     hitable *world = new hitable_list(list, NUM_ELEMENTS);
-    camera cam(vec3(-2, 2, 1), vec3(0, 0, -1), vec3(0, 1, 0), 10, float(nx)/float(ny));
+
+    vec3 lookfrom(3,3,2);
+    vec3 lookat(0,0,-1);
+    float dist_to_focus = (lookfrom - lookat).length();
+    float aperture = 2.0;
+    camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), aperture, dist_to_focus);
 
     for (int j = ny-1; j >=0; j--) {
         for (int i = 0; i < nx; i++) {
